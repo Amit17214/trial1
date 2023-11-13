f6cefc80549f74fe97e37e6e88e6082b91515bec
Amit Singh <amit17121999@gmail.com>
2023-11-13 13:48:56 +0530

    changes

--- src/Create.js
+++ src/Create.js
@@ -1,38 +1,50 @@
 import React, { useState } from "react";
 import Select from "react-select";
-
+ 
 const Create = () => {
-    const date = new Date();
-    const todayDate = date.toDateString();
-    const today = date.getDate();
-    const todayMonth = date.getMonth();
-
+    let date = new Date();
+    let todayDate = date.toDateString();
+    let today = date.getDate();
+    let todayMonth = date.getMonth();
+ 
     const monthArr = [
-        "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
     ];
-
+ 
     const [holidays, setHolidays] = useState(0);
-    const [vac, setVac] = useState([]);
-
-    const updateDays = (e) => {
-        const sortedVac = e.sort((a, b) => a.value - b.value);
-        console.log(sortedVac);
-        setVac(sortedVac);
-    };
-
+ 
     const options = Array.from({ length: holidays }, (_, index) => ({
         value: index + 1,
         label: `Day ${index + 1}`,
     }));
-
+ 
+    const [vac, setVac] = useState([]);
+ 
+    function updateDays(e) {
+        // Sort the selected days based on the 'value' property
+        const sortedVac = e.sort((a, b) => a.value - b.value);
+        setVac(sortedVac);
+    }
+ 
     return (
         <div>
             <span>Today's Date: {todayDate}</span>
-
+ 
             <br />
             <br />
-
-            <label htmlFor="holiday">Stay Nights : </label>
+ 
+            <label htmlFor="holiday">Total Holiday Days: </label>
             <br />
             <input
                 type="number"
@@ -43,23 +55,39 @@ const Create = () => {
                 value={holidays}
                 onChange={(e) => setHolidays(e.target.value)}
             />
-
+ 
             <br />
             <br />
-
-            <Select
-                isMulti
-                options={options}
-                onChange={(selected) => updateDays(selected)}
-            />
-
-            {vac.map(({ value }, index) => (
-                <p key={index}>
-                    {today + value - 1} {monthArr[todayMonth]}
-                </p>
-            ))}
+ 
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <Select
+                    isMulti
+                    options={options}
+                    onChange={(selected) => updateDays(selected)}
+                />
+            </div>
+            <div>
+                {/* Map over the sorted 'vac' array */}
+                <div
+                    style={{
+                        border: "2px solid black",
+                        backgroundColor:"alice-blue",
+                        width: "300px",
+                        marginLeft: "20%",
+                        marginTop: "50px",
+                        padding: "10px",
+                    }}>
+                    {vac.map((val, index) => {
+                        return (
+                            <p key={index}>
+                                {today + val.value - 1} {monthArr[todayMonth]}
+                            </p>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
     );
 };
-
+ 
 export default Create;
